72e160136271f5f91c1962bb2d5bf2d634bbab7d
obliviga <obliviga@gmail.com>
2014-11-05 15:19:56 -0500

    Able to login to Wordpress sample project; able to assert if on Dashboard page

---
+++ Automation/Driver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Automation
+{
+    public class Driver
+    {
+        public static IWebDriver Instance { get; set; }
+
+        public static void Initialize()
+        {
+            Instance = new FirefoxDriver();
+            Instance.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+        }
+
+        public static void Close()
+        {
+            Instance.Close();
+        }
+    }
+}
---
+++ Tests/LoginTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Automation;
+
+namespace Tests
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Driver.Initialize();
+        }
+
+        [TestMethod]
+        public void Admin_User_Can_Login()
+        {
+            LoginPage.GoTo();
+            LoginPage.LoginAs("admin").WithPassword("password").Login();
+
+            Assert.IsTrue(DashboardPage.IsAt, "Failed to login.");
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Driver.Close();
+        }
+    }
+}
