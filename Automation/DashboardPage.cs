7b429581ea6604c1a31426b2dc1993cb0bfada7e
obliviga <obliviga@gmail.com>
2014-11-17 00:59:40 -0500

    Testing if git will stop asking me for a password for each commit

--- Automation/DashboardPage.cs
+++ Automation/DashboardPage.cs
@@ -14,7 +14,7 @@ namespace Automation
                 var h2s = Driver.Instance.FindElements(By.TagName("h2"));
                 if(h2s.Count > 0)
                     return h2s[0].Text == "Dashboard";
-                return false;
+                return false; 
             }
         }
     }
